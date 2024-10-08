cb8dc6ad5b0ac16a1414189604f89b85ad0fd662
Lorenzo Ramirez <lenchetoramirez16@gmail.com>
2024-10-08 13:32:08 -0400

    refactor (settings page): adds new widget design and unnecessary files
    NOTE: disregard React plan previously mentioned

--- scripts/global-functions.js
+++ scripts/global-functions.js
@@ -1,56 +1,63 @@
-
 /**
  * SECTION - STORAGE RELATED
  */
 
 /**
  * Retrieve settings from storage.
- * 
+ *
  * @param {string} key - The key of the setting to retrieve.
  * @param {Function} callback - The function to call with the retrieved value.
- * 
+ *
  * @returns {void} This function does not return anything. It calls the callback with the retrieved value.
- * 
+ *
  * @example getSettings('myKey', function(value) { console.log(value); });
  */
 window.getSettings = (key, callback) => {
-  chrome.storage.sync.get([key][0], (result) => {
-    callback(result[key]);
-  });
-}
+  try {
+    chrome.storage.sync.get([key][0], (result) => {
+      callback(result[key]);
+    });
+  } catch (error) {
+    console.log(`Error getting ${key} ${error}`);
+  }
+};
 
 /**
  * Sets a specific setting in storage and logs the change.
- * 
+ *
  * @param {string} key - The key of the setting to set.
  * @param {any} value - The value to set for the given key.
- * 
+ *
  * @returns {void} This function does not return anything. It sets a value in storage and logs the change.
- * 
+ *
  * @example setSetting('myKey', 'true');
  */
 window.setSetting = (key, value) => {
   let save = {};
   save[key] = value;
-  chrome.storage.sync.set(save, function() {
+  chrome.storage.sync.set(save, function () {
     getSettings(key, (result) => {
-      console.log(`SETTINGS CHANGED: ${key} setting was changed to ${result[key]}`);
+      console.log(
+        `SETTINGS CHANGED: ${key} setting was changed to ${result[key]}`
+      );
     });
   });
-}
+};
 
 window.setNestedSetting = (key, subKey, value, callback) => {
-  chrome.storage.sync.get(key, function(result) {
+  chrome.storage.sync.get(key, function (result) {
     if (!result[key]) {
       result[key] = {};
     }
     result[key][subKey] = value;
     let save = {};
     save[key] = result[key];
-    chrome.storage.sync.set(save, function() {
+    chrome.storage.sync.set(save, function () {
       getSettings(key, (result) => {
-        console.log(`SETTINGS CHANGED: ${key}.${subKey} setting was changed to ${result[subKey]}`);
-        if (typeof callback === 'function') {
+        console.log(
+          `SETTINGS CHANGED: ${key}.${subKey} setting was changed to ${result[subKey]}`
+        );
+        if (typeof callback === "function") {
           callback(result); // Execute the callback, passing the result as an argument
         }
       });
@@ -60,39 +67,42 @@ window.setNestedSetting = (key, subKey, value, callback) => {
 
 /**!SECTION */
 
-
 /**
  * SECTION - TIME USAGE RELATED
- *  
+ *
  */
 /**
  * Converts time usage into an accurate time statement
- * 
+ *
  * @param {int} timeUsage - the value of the time usage storage value
- * 
+ *
  * @returns {string} Returns time usage in a statement that is accurate to the amount of time given
- * Time statements are dynamic meaning depending on the amount of time, they will include 
- *  time measurements (mins, hours, etc.) when needed  
- * 
+ * Time statements are dynamic meaning depending on the amount of time, they will include
+ *  time measurements (mins, hours, etc.) when needed
+ *
  * @example let usageStatement = convertTimeToText(timeUsage);
  */
 window.convertTimeToText = (timeUsage) => {
   // if time usage is below a minute
   if (timeUsage < 60) {
     return `${timeUsage} seconds`;
-  } else if (timeUsage >= 60) { // if time usage is above a minute
+  } else if (timeUsage >= 60) {
+    // if time usage is above a minute
     let min = Math.floor(timeUsage / 60);
     let sec = Math.floor(timeUsage - min * 60);
 
     // if time usage is between a minute and an hour
     if (timeUsage < 3600) {
       return `${min} ${min === 1 ? "Minute" : "Minutes"} ${sec} Seconds`;
-    } else if (timeUsage >= 3600) { // if time usage is above an hour
+    } else if (timeUsage >= 3600) {
+      // if time usage is above an hour
       let hours = Math.floor(timeUsage / 3600);
-      let remainingMinutes = Math.floor((timeUsage - (hours * 3600)) / 60);
-      return `${hours} Hr${hours !== 1 ? 's' : ''} ${remainingMinutes} ${remainingMinutes === 1 ? 'Min' : 'Mins'} ${sec} ${sec === 1 ? 'Sec' : 'Secs'}`;
+      let remainingMinutes = Math.floor((timeUsage - hours * 3600) / 60);
+      return `${hours} Hr${hours !== 1 ? "s" : ""} ${remainingMinutes} ${
+        remainingMinutes === 1 ? "Min" : "Mins"
+      } ${sec} ${sec === 1 ? "Sec" : "Secs"}`;
     }
   }
-}
+};
 
 /**!SECTION */
--- scripts/settings-general.js
+++ scripts/settings-general.js
@@ -1,8 +1,8 @@
 /**
- * @LenchetoFC 
+ * @LenchetoFC
  * @description This controls the settings pages and accurately
  *  displays current settings and user information for the general settings
- * 
+ *
  */
 
 /**
@@ -19,38 +19,54 @@
 //        to be able to use the values outside of the callback
 
 // For determining if a YT element either fades or slides out of yt page examples
-const ytFadeToggleElements = ["all-pages", "home-page", "search-bar", "shorts-btn"]
+const ytFadeToggleElements = [
+  "all-pages",
+  "home-page",
+  "search-bar",
+  "shorts-btn",
+];
 
 // Adds all activities from storage to HTML on load
 retrieveActivityFromStorage();
 
 // Gets initial num of activities and displays add button if not reached max
-getSettings("activities", (result) => {
-  let activityNum = 0;
-  try {
-    activityNum = result.length;
-  } catch {
-    activityNum = 0;
-  }
-});
+try {
+  getSettings("activities", (result) => {
+    let activityNum = 0;
+    try {
+      activityNum = result.length;
+    } catch {
+      activityNum = 0;
+    }
+  });
+} catch (error) {
+  console.log(`Error getting activities ${error}`);
+}
 
 // Displays current watch times in HTML
-getSettings("watch-usage", (result) => {
-  $('#all-time-count').text(convertTimeToText(result['all-time']));
-  $('#regular-time-count').text(convertTimeToText(result['regular-video']));
-  $('#shorts-time-count').text(convertTimeToText(result['shorts']));
-});
+// try {
+//   getSettings("watch-usage", (result) => {
+//     $("#all-time-count").text(convertTimeToText(result["all-time"]));
+//     $("#regular-time-count").text(convertTimeToText(result["regular-video"]));
+//     $("#shorts-time-count").text(convertTimeToText(result["shorts"]));
+//   });
+// } catch (error) {
+//   console.log(`Error getting watch times ${error}`);
+// }
 
 // Gets initial num of activities and displays add button if not reached max
-getSettings("activities", (result) => {
-  let activityNum = 0;
-  try {
-    activityNum = result.length;
-  } catch {
-    activityNum = 0;
-  }
-});
-
+try {
+  getSettings("activities", (result) => {
+    let activityNum = 0;
+    try {
+      activityNum = result.length;
+    } catch {
+      activityNum = 0;
+    }
+  });
+} catch (error) {
+  console.log(`Error getting activities ${error}`);
+}
 
 /**
  * SECTION - FUNCTION DECLARATIONS
@@ -59,13 +75,17 @@ getSettings("activities", (result) => {
 
 /** FUNCTION - retrieves the 'activities' setting and adds each activity to the HTML */
 function retrieveActivityFromStorage() {
-  getSettings("activities", (result) => {
-    if (result != undefined) {
-      result.forEach((element, index) => {
-        insertActivityHTML(element);
-      });
-    }
-  });
+  try {
+    getSettings("activities", (result) => {
+      if (result != undefined) {
+        result.forEach((element, index) => {
+          insertActivityHTML(element);
+        });
+      }
+    });
+  } catch (error) {
+    console.log(`Error getting activities ${error}`);
+  }
 }
 
 /** FUNCTION - Creates a new HTML element for an activity and adds it to the 'activity-section' in the document */
@@ -79,137 +99,162 @@ function insertActivityHTML(activity) {
       </div>
   `);
 
-  activityItem.css("display", "none")
+  activityItem.css("display", "none");
 
-  $('#activity-section').append(activityItem);
+  $("#activity-section").append(activityItem);
   activityItem.slideDown();
-};
+}
 
 /** FUNCTION - Adds an activity to the 'activities' setting in storage. If the 'activities' setting does not exist, it creates a new one */
-function addActivityStorage (activity) {
-  getSettings("activities", (result) => {
-    // Saves new activity to exisiting storage 
-    result.unshift(activity);
-    setSetting("activities", result);
-  });
+function addActivityStorage(activity) {
+  try {
+    getSettings("activities", (result) => {
+      // Saves new activity to exisiting storage
+      result.unshift(activity);
+      setSetting("activities", result);
+    });
+  } catch (error) {
+    console.log(`Error getting activities ${error}`);
+  }
 }
 
 /** FUNCTION - Removes a specific activity from the 'activities' setting in storage */
-function removeActivity (value) {
-  getSettings("activities", (result) => {
-    // Get index of key activity and removes it
-    // let activityIndex = result.indexOf(activity);
-    result.splice(result.indexOf(value), 1);
+function removeActivity(value) {
+  try {
+    getSettings("activities", (result) => {
+      // Get index of key activity and removes it
+      // let activityIndex = result.indexOf(activity);
+      result.splice(result.indexOf(value), 1);
 
-    // Saves updated array to storage 
-    setSetting("activities", result);
-  });
+      // Saves updated array to storage
+      setSetting("activities", result);
+    });
+  } catch (error) {
+    console.log(`Error getting activities ${error}`);
+  }
 }
 
 /** FUNCTION - Inserts activity from storage into HTML to and adds them to storage */
-// NOTE: Not exactly sure if this is even necessary 
+// NOTE: Not exactly sure if this is even necessary
 function addActivityEventHandler() {
   let activityNumEvent = 0;
   let activityInput = $("#activity-input");
 
   // Gets current number of activities
-  getSettings("activities", (result) => {
-    try {
-      activityNumEvent = result.length;
-    } catch {
-      activityNumEvent = 0;
-    }
+  try {
+    getSettings("activities", (result) => {
+      try {
+        activityNumEvent = result.length;
+      } catch {
+        activityNumEvent = 0;
+      }
 
-    if(activityInput.val().length > 0 && activityNumEvent < 4){
-      insertActivityHTML(activityInput.val());
-      addActivityStorage(activityInput.val());
-      activityInput.val("");
-    }
-  });
+      if (activityInput.val().length > 0 && activityNumEvent < 4) {
+        insertActivityHTML(activityInput.val());
+        addActivityStorage(activityInput.val());
+        activityInput.val("");
+      }
+    });
+  } catch (error) {
+    console.log(`Error getting activities ${error}`);
+  }
 }
 
 /** FUNCTION - Displays or hides 'add activity' button or "activity input" only under the right conditions */
 function toggleActivityBtns() {
-  let activityItemAmt = $('#activity-section').children().length;
+  let activityItemAmt = $("#activity-section").children().length;
 
   console.log(activityItemAmt);
-  console.log($('#input-btn-box').css("visibility"))
-  
-  if (activityItemAmt < 4 && $('#input-btn-box').css("visibility") == "visible") {
+  console.log($("#input-btn-box").css("visibility"));
+
+  if (
+    activityItemAmt < 4 &&
+    $("#input-btn-box").css("visibility") == "visible"
+  ) {
     // $('#activity-add').css("display", "block");
     // $('#activity-add').css("visibility", "visible");
     // $('#input-btn-box').css("visibility", "visible");
-    $('#activity-add').slideToggle();
-    $('#input-btn-box').slideToggle();
-  } else if (activityItemAmt < 4 && $('#input-btn-box').css("visibility") == "hidden") {
+    $("#activity-add").slideToggle();
+    $("#input-btn-box").slideToggle();
+  } else if (
+    activityItemAmt < 4 &&
+    $("#input-btn-box").css("visibility") == "hidden"
+  ) {
     // $('#activity-add').css("display", "none");
     // $('#activity-add').css("visibility", "hidden");
-    $('#activity-add').slideToggle();
-    $('#input-btn-box').slideToggle();
+    $("#activity-add").slideToggle();
+    $("#input-btn-box").slideToggle();
   } else {
-
   }
 }
 
 /** FUNCTION - Displays or hides activity inputs based on amount of active activities */
-function showActivityInput () {
-  let activityItemAmt = $('#activity-section').length;
-  
-  if (activityItemAmt < 4 && $('#input-btn-box').css("visibility") == "hidden") {
-    $('#activity-add').css("display", "block");
-    $('#activity-add').css("visibility", "visible");
-    $('#input-btn-box').css("visibility", "visible");
+function showActivityInput() {
+  let activityItemAmt = $("#activity-section").length;
+
+  if (
+    activityItemAmt < 4 &&
+    $("#input-btn-box").css("visibility") == "hidden"
+  ) {
+    $("#activity-add").css("display", "block");
+    $("#activity-add").css("visibility", "visible");
+    $("#input-btn-box").css("visibility", "visible");
   } else {
-    $('#activity-add').css("display", "none");
-    $('#activity-add').css("visibility", "hidden");
+    $("#activity-add").css("display", "none");
+    $("#activity-add").css("visibility", "hidden");
   }
 }
 
 /** FUNCTION - Toggles all checkboxes based on the settings value (true == checked or false == unchecked) */
 function toggleCheckboxes(setting, element) {
-  getSettings(setting, (result) => {
-    // Visually displays the status of the setting on load
-    if (result[element.value]) {
-      element.checked = true; // Auto-updates checkbox status
-      $(`.${element.value}`).slideToggle() // Auto-updates YT UI example
-    } else {
-      element.checked = false;
-    }
+  try {
+    getSettings(setting, (result) => {
+      // Visually displays the status of the setting on load
+      if (result[element.value]) {
+        element.checked = true; // Auto-updates checkbox status
+        $(`.${element.value}`).slideToggle(); // Auto-updates YT UI example
+      } else {
+        element.checked = false;
+      }
 
-    // Updates settings for whichever button is pushed
-    element.addEventListener("click", (event) => {
-      setNestedSetting(setting, element.name, element.checked);
+      // Updates settings for whichever button is pushed
+      element.addEventListener("click", (event) => {
+        setNestedSetting(setting, element.name, element.checked);
 
-      // Displays change in YT UI example
-      ytFadeToggleElements.includes(element.value) ? $(`.${element.value}`).fadeToggle() : $(`.${element.value}`).slideToggle() 
+        // Displays change in YT UI example
+        ytFadeToggleElements.includes(element.value)
+          ? $(`.${element.value}`).fadeToggle()
+          : $(`.${element.value}`).slideToggle();
+      });
     });
-  });
+  } catch (error) {
+    console.log(`Error getting settings ${error}`);
+  }
 }
 
-
-/** 
- * SECTION - EVENT LISTENERS 
- * 
+/**
+ * SECTION - EVENT LISTENERS
+ *
  */
 
 // Deletes activity through 'x' button
-$(document).on('click', '.icon-delete', function(event) {
-  const activityItem = $(this).closest('li');
-  const activityItemId = $(this).closest('div').attr('id');
+$(document).on("click", ".icon-delete", function (event) {
+  const activityItem = $(this).closest("li");
+  const activityItemId = $(this).closest("div").attr("id");
 
   removeActivity(activityItemId);
 
   if (activityItem.length) {
-    activityItem.slideUp(function() {
+    activityItem.slideUp(function () {
       $(this).remove();
     });
   }
 });
 
 // Triggers toggleCheckboxes with the type of checkbox on every checkbox within a form
-const addictiveForm = document.querySelectorAll("form input");
+const addictiveForm = document.querySelectorAll("fieldset input");
 addictiveForm.forEach((element) => {
-  if (element.name.includes('quick')) {
+  if (element.name.includes("quick")) {
     toggleCheckboxes("quick-actions", element);
   } else {
     toggleCheckboxes("addictive-elements", element);
@@ -217,20 +262,20 @@ addictiveForm.forEach((element) => {
 });
 
 // Displays activity input text box and auto-focuses on it
-$('#activity-add button').on("click", function() {
-  $('#input-btn-box').slideDown( function() {
-    $('#input-container').trigger("focus");
+$("#activity-add button").on("click", function () {
+  $("#input-btn-box").slideDown(function () {
+    $("#input-container").trigger("focus");
   });
-  $('#activity-add').slideUp();
+  $("#activity-add").slideUp();
 });
 
 // Removes activity input and reshows add activity button
-$('#activity-cancel').on("click", function() {
+$("#activity-cancel").on("click", function () {
   toggleActivityBtns();
 });
 
 // "Enter" event listener for new activity
-$('#activity-input').on("keydown", function ( event ) {
+$("#activity-input").on("keydown", function (event) {
   let inputTextLen = $(this).val().length;
   if (event.which == 13 && inputTextLen > 0) {
     addActivityEventHandler();
@@ -239,27 +284,27 @@ $('#activity-input').on("keydown", function ( event ) {
 });
 
 // "Save button" event listener for new activity
-$('#activity-save').on("click", function() {
-  let inputTextLen = $('#activity-input').val().length;
+$("#activity-save").on("click", function () {
+  let inputTextLen = $("#activity-input").val().length;
   if (inputTextLen > 0) {
     addActivityEventHandler();
     toggleActivityBtns();
   }
-})
+});
 
 // Resets all time usage to 0 and updates the displayed count
-$('#reset-usage').on("click", function() {
+$("#reset-usage").on("click", function () {
   setSetting("all-time-usage", 0);
-  $('#all-time-count').text('0 seconds');
+  $("#all-time-count").text("0 seconds");
   console.log("ALL TIME USAGE RESET TO 0");
-})
+});
 
 // Opens and closes horizontal nav bar
 // document.querySelector(".hamburger-input").addEventListener("click", () => {
 //   let checked = document.querySelector(".hamburger-input").checked;
 //   let nav = document.querySelector("nav");
 //   let contentWrapper = document.querySelector(".content-wrapper");
-  
+
 //   if (window.matchMedia("(max-width: 630px)").matches) {
 //     if (checked) {
 //       nav.style.transform = "translateY(83.5px)";
